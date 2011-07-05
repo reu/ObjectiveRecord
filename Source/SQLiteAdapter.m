10f0b1e21be2c4035871300ee82f07e07af19abb
Rodrigo Navarro <rnavarro1@gmail.com>
2011-07-05 12:06:09 -0300

    Calling super init on custom initializers (noob)

--- Source/SQLiteAdapter.m
+++ Source/SQLiteAdapter.m
@@ -22,14 +22,16 @@
 @implementation SQLiteAdapter
 
 - (id)initWithPath:(NSString *)path {
-    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-        if (sqlite3_open([path UTF8String], &database) != SQLITE_OK) {
-            [NSException raise:@"The informed path is not a valid sqlite3 database" 
-                        format:@"couldn't connect to %d", path];
+    if (self = [super init]) {
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+            if (sqlite3_open([path UTF8String], &database) != SQLITE_OK) {
+                [NSException raise:@"The informed path is not a valid sqlite3 database" 
+                            format:@"couldn't connect to %d", path];
+            }
+        } else {
+            [NSException raise:@"You must specify the path to a pre-existing sqlite database" 
+                         format:@"%d is not a sqlite database", path];
         }
-    } else {
-        [NSException raise:@"You must specify the path to a pre-existing sqlite database" 
-                     format:@"%d is not a sqlite database", path];
     }
     
     return self;
